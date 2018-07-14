b5da1fba312fb013313aa2bad4100013d4056dc7
climberger <carsten.limberger@gmx.net>
2018-07-14 19:52:23 +0200

    Add some TODOs.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,3 +14,13 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// TODO: Add module structure with core module, shared module and feature modules and related routing modules
+// TODO: Add own navbar as component
+// TODO: Add redux, redux router, redux form, redux logging and redux devkit
+// TODO: Add own redux list store functionality
+// TODO: Add own redux dictionary
+// TODO: Add own redux set state property functionality
+// TODO: Add own logging service
+// TODO: Add own translation pipe
+// TODO: Add own generic table
